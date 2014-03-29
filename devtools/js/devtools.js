80ae6ef02078418f48840b64d8056ca7d8141cd1
PiHiZi <pihizi@msn.com>
2014-03-29 15:49:05 +0800

    init devtools

---
+++ devtools/js/devtools.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var pluginTitle = 'Name';
+
+// The following code adds a sidebar pane contained in sidebar.html and titled pluginTitle to the Elements panel
+chrome.devtools.panels.elements.createSidebarPane(pluginTitle, function(sidebar) {
+    sidebar.setPage('devtools/sidebar.html');
+    sidebar.setHeight('100%');
+});
+
+/*
+// The following code adds a panel contained in panel.html, represented by png on the Developer Tools toolbar and labeled as Font Picker:
+chrome.devtools.panels.create(pluginTitle, 'icons/icon.48.png', 'devtools/panel.html', function(panel) {
+});
+*/
